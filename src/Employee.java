cc5e9e7f5667977e603b44113bf688af186d69e5
harshu009778 <harshu.19791@gmail.com>
2025-12-08 18:03:53 +0530

    Added : Part Time

--- src/Employee.java
+++ src/Employee.java
@@ -1,7 +1,4 @@
 public class Employee {
-
-
-
     int dailyAttendance;
     int workPerDay = 8;
     int halfDay = 4;
@@ -9,18 +6,27 @@ public class Employee {
     int dailySalary = 0;
 
      public void calculateAttendance(){
-         if(Math.random() < 0.5){
+         if(((Math.random()*100)%3 )==0){
              dailyAttendance = 0;
              System.out.println("Absent");
          }
-         else{
+
+         else if(((Math.random()*100)%3 )==1){
              dailyAttendance = 1;
+             System.out.println("Part Time");
+         }
+         else{
+             dailyAttendance = 2;
              System.out.println("Present");
          }
      }
 
      public void dailyWage(){
          if(dailyAttendance == 1){
+             dailySalary = halfDay * hourlyPay;
+             System.out.println(dailySalary);
+         }
+         else if(dailyAttendance == 2){
              dailySalary = workPerDay * hourlyPay;
              System.out.println(dailySalary);
          }
