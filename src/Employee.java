2a61c3dfb0cba89217b237debb6e8b58bc64df83
harshu009778 <harshu.19791@gmail.com>
2025-12-08 17:50:17 +0530

    Initial Push

---
+++ src/Employee.java
@@ -0,0 +1,33 @@
+public class Employee {
+
+
+
+    int dailyAttendance;
+    int workPerDay = 8;
+    int halfDay = 4;
+    int hourlyPay = 20;
+    int dailySalary = 0;
+
+     public void calculateAttendance(){
+         if(Math.random() < 0.5){
+             dailyAttendance = 0;
+             System.out.println("Absent");
+         }
+         else{
+             dailyAttendance = 1;
+             System.out.println("Present");
+         }
+     }
+
+     public void dailyWage(){
+         if(dailyAttendance == 1){
+             dailySalary = workPerDay * hourlyPay;
+             System.out.println(dailySalary);
+         }
+         else{
+             System.out.println("No pay");
+         }
+     }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+        e1.calculateAttendance();;
+        e1.dailyWage();
+    }
+}
