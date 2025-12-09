fcf972d4f8fc78da4b6ebd579d9b077213a7345c
harshu009778 <harshu.19791@gmail.com>
2025-12-09 16:05:11 +0530

    Added : Wage according to condition

--- src/Employee.java
+++ src/Employee.java
@@ -6,6 +6,11 @@ public class Employee {
     int dailySalary = 0;
     int monthlySalary = 0;
 
+    int maxHours = 100;
+    int maxDays = 20;
+    int totalHours = 0;
+    double totalDays = 0;
+
     // --------------UC1 --------------
      public void calculateAttendance(){
          if(((Math.random()*100)%3 )==0){
@@ -22,6 +27,7 @@ public class Employee {
              System.out.println("Present");
          }
      }
+
 
      // --------------UC2 & UC3 --------------
      public void dailyWage(){
@@ -40,7 +46,7 @@ public class Employee {
 
      //--------------- UC4 ---------------
      public int calAttendanceUsingSwitch(){
-         int check = (int)(Math.random()*3);
+         int check = (int)(Math.random()*3); // 0,1,2
          dailyAttendance = check;
 
          switch (check){
@@ -90,4 +96,24 @@ public class Employee {
         System.out.println("Total Monthly Salary = " + monthlySalary);
     }
 
+    // -------------- UC6: Wage till 100 hours OR 20 days --------------
+        public void wageUnderCondition(){
+            while(totalDays <= maxDays || (totalHours <= maxHours)){
+                int dailyStatus = calAttendanceUsingSwitch();
+                if(dailyStatus == 0){
+                    totalHours += 0;
+                }
+                else if(dailyStatus == 1){ // part time
+                    totalHours += 4;
+                }
+                else if (dailyStatus == 2) { // full time
+                    totalHours += 8;
+                }
+                totalDays++;
+
+            }
+
+            int wages = totalHours * hourlyPay;
+            System.out.println("Wages : " + wages);
+        }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ public class Main {
 
     public static void main(String[] args) {
         Employee e1 = new Employee();
+
+        // ------- UC1 ---------
         //e1.calculateAttendance();;
+
+        // ------ UC2 & UC3 ------------
         //e1.dailyWage();
 
         //--------- UC4 ----------
@@ -12,5 +16,8 @@ public class Main {
 
         //--------- UC5 -----------
         e1.monthlyWage();
+
+        // -------------- UC6: Wage till 100 hours OR 20 days --------------
+        e1.wageUnderCondition();
     }
 }
